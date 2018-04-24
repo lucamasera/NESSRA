0c179c2f393e5809c6aa83c0ed63ec609ada6de5
Luca <luca.masera@live.com>
2018-04-24 10:43:12 +0200

    removed windows support attempt

--- run_NESSRA_sample.sh
+++ run_NESSRA_sample.sh
@@ -2,4 +2,4 @@
 
 cd pc && make && cd ..
 
-./NESSRA.py -l sample_data/lgn_input -d sample_data/experiments.csv -t 500 1000 -i 10 20 -a 0.0001 0.0005 -p pc/bin/pc++ -o test  -n 4 -k 100
+./NESSRA.py -l sample_data/lgn_input -d sample_data/experiments.csv -t 1500 1000 -i 1 2 -a 0.0001 0.0005 -p pc/bin/pc++ -o test  -n 4 -k 100
