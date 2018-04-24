b47e05f1b042478aff8f1682891516ebf4f35b46
lucamasera <lucamasera@users.noreply.github.com>
2018-04-24 14:15:33 +0200

    sample to example

--- run_NESSRA_example.sh
+++ run_NESSRA_example.sh
@@ -2,4 +2,4 @@
 
 cd pc && make && cd ..
 
-./NESSRA.py -l sample_data/lgn_input -d sample_data/experiments.csv -t 1500 1000 -i 1 2 -a 0.0001 0.0005 -p pc/bin/pc++ -o test  -n 4 -k 100
+./NESSRA.py -l example_data/lgn_input -d example_data/experiments.csv -t 1500 1000 -i 1 2 -a 0.0001 0.0005 -p pc/bin/pc++ -o test  -n 4 -k 100
